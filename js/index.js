10c040bf68cb2c113f7a3a17561ac3d4986b7621
alebusi <allebusi@gmail.com>
2018-02-17 11:12:27 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ function generateVariables (){
 
 	targhet_canvas_id = "pageCanvas"; // il canvas in cui vogliamo disegnare
 	lati = getRandomInt(1, 8) ; // numro dei lati del poligono, se x < 3 verra diasegnato un cerchio, per una selezone random usare "getRandomInt(1, 8)"
-	diametro_costruzione_poligono = 100; // grandezza poligono 
+	diametro_costruzione_poligono = 200; // grandezza poligono 
 	diametro_costruzione_cerchio = 50; // grandezza cerchio centrale
 	rotazione = Math.random() *Math.PI/lati; // rotazione del poligono, "Math.PI/lati" serve per tenere le facce ortogonali alla finestra, "Math.random() *Math.PI*2" per una rotazione random
 	
