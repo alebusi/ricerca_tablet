87b8c12312f821eaa317b56cb768326863ba1af0
alebusi <allebusi@gmail.com>
2018-03-01 12:32:22 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -35,8 +35,8 @@ function generateVariables (){
 	diametro_costruzione_cerchio = 50; // grandezza cerchio centrale
 	rotazione = Math.PI/lati; // rotazione del poligono, "Math.PI/lati" serve per tenere le facce ortogonali alla finestra, "Math.random() *Math.PI*2" per una rotazione random
 	
-	colore_poligono= "#000000";
-	colore_cerchio = "#FF3300";
+	colore_poligono= "#FFFFFF";  //"#000000";
+	colore_cerchio = "#0000E0";  //"#FF3300";
 
 	pos_x =((Math.random() * 100) + 1 )/100; // posizione x e y random del poligono
 	pos_y = ((Math.random() * 100) + 1 )/100; 
@@ -220,8 +220,8 @@ function drawPoligon(targhet_canvas_id ,pos_x,pos_y , n_faces, outer_diameter, r
 	canvas_context.lineWidth = 0; // spessore del filetto
 	canvas_context.fillStyle = colour; // colore di riempimento
 	canvas_context.fill();
-	//canvas_context.strokeStyle = "#000000"; // colore del filetto
-	canvas_context.strokeStyle = colour; // colore del filetto
+	canvas_context.strokeStyle = "#000000"; // colore del filetto
+	// canvas_context.strokeStyle = colour; // colore del filetto
     canvas_context.stroke();
 
 }
